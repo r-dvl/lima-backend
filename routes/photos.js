8dcf7a413f99ecb20fe4cfd4a2844647412ea8af
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-18 17:03:53 +0200

    Simplify to just an API Rest

--- routes/photos.js
+++ routes/photos.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
 const Photo = require('../models/Photo');
 
+// API Routes
 router.get('/', async (req, res) => {
     try {
         const photos = await Photo.find();
-        res.render('photos', { photos });
+        const formattedPhotos = photos.map(photo => ({
+            date: photo.date.toString(),
+            image: photo.image
+        }));
+        res.json(formattedPhotos);
     } catch (error) {
-        res.status(500).json({ error: 'Error obtaining photos from server.' });
+        res.status(500).json({ error: 'Error obtaining photos from server' });
     }
 });
 
