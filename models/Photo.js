fefda06ce5acb6e731dac8ab7e3cb78bfd32c220
R-dVL <rauldel.valle.lima@gmail.com>
2023-08-24 10:45:29 +0200

    New http requests

--- models/Photo.js
+++ models/Photo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     date: Date,
     image: String
 });
--- routes/photos.js
+++ routes/photos.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Photo = require('../models/Photo');
 
 // API Routes
+// Get all
 router.get('/', async (req, res) => {
     try {
         const photos = await Photo.find();
@@ -20,8 +21,9 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:photoId', async (req, res) => {
-    const photoId = req.params.photoId;
+// Get by id
+router.get('/:id', async (req, res) => {
+    const photoId = req.params.id;
 
     try {
         const photo = await Photo.findById(photoId);
@@ -34,6 +36,61 @@ router.get('/:photoId', async (req, res) => {
     } catch (error) {
         console.error('Error fetching photo:', error);
         res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// Get by date
+router.get('/date/:date', async (req, res) => {
+    const requestedDate = new Date(req.params.date);
+
+    const nextDay = new Date(requestedDate);
+    nextDay.setDate(nextDay.getDate() + 1);
+
+    try {
+        const photos = await Photo.find({
+            date: {
+                $gte: requestedDate,
+                $lt: nextDay,
+            },
+        }).exec();
+        res.json(photos);
+    } catch (err) {
+        res.status(500).json({ error: 'Error obtaining photos' });
+    }
+});
+
+// Delete by date (Day deleted)
+router.delete('/date/:date', async (req, res) => {
+    const requestedDate = new Date(req.params.date);
+
+    try {
+        const result = await Photo.deleteMany({
+            date: {
+                $gte: requestedDate,
+                $lt: new Date(requestedDate.getTime() + 24 * 60 * 60 * 1000), // Next day
+            },
+        }).exec();
+
+        res.json({ message: `${result.deletedCount} photos deleted` });
+    } catch (err) {
+        res.status(500).json({ error: 'Error deleting photos' });
+    }
+});
+
+// Post photo
+router.post('/upload', async (req, res) => {
+    try {
+        const { date, image } = req.body;
+
+        const newPhoto = new Photo({
+            date: new Date(date),
+            image: image,
+        });
+
+        await newPhoto.save();
+        res.json({ message: 'Photo uploaded' });
+    } catch (err) {
+        res.status(500).json({ error: err.toString() });
     }
 });
 
